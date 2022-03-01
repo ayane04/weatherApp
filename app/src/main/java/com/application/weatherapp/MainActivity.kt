b49a9a0e79d813847ff1a2f67b03c7f3155312d1
ayane04 <89826012+ayane04@users.noreply.github.com>
2022-02-28 20:55:54 -0800

    dropdown

--- app/src/main/java/com/application/weatherapp/MainActivity.kt
+++ app/src/main/java/com/application/weatherapp/MainActivity.kt
@@ -3,23 +3,64 @@ package com.application.weatherapp
 import android.os.AsyncTask
 import android.os.Bundle
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
+import android.widget.Spinner
+import android.widget.ArrayAdapter
+import com.application.weatherapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    val CITY: String = "Tokyo"
-    val API: String = "881347715cccdd757623c4b7c8ec5343"
+    val CITY: String = "Tokyo, JP"
+    val API: String = "06c921750b9a82d8f5d1294e1586276f"
+
+    private lateinit var binding: ActivityMainBinding
+    private val spinnerItems = arrayOf("Select city", "Tokyo", "California", "London")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val spinner = findViewById<Spinner>(R.id.spinner)
+//        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerItems)
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        spinner.adapter = adapter
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // ArrayAdapter
+        val adapter = ArrayAdapter(applicationContext,
+            android.R.layout.simple_spinner_item, spinnerItems)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // spinner に adapter をセット
+        // View Binding
+        binding.spinner.adapter = adapter
+
+        // リスナーを登録
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            //　アイテムが選択された時
+            override fun onItemSelected(parent: AdapterView<*>?,
+                                        view: View?, position: Int, id: Long) {
+                val spinnerParent = parent as Spinner
+                val item = spinnerParent.selectedItem as String
+                // View Binding
+                binding.textView.text = item
+            }
+
+            //　アイテムが選択されなかった
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //
+            }
+        }
 
         weatherTask().execute()
 
@@ -28,6 +69,7 @@ class MainActivity : AppCompatActivity() {
     inner class weatherTask() : AsyncTask<String, Void, String>() {
         override fun onPreExecute() {
             super.onPreExecute()
+            /* Showing the ProgressBar, Making the main design GONE */
             findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
             findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.GONE
             findViewById<TextView>(R.id.errorText).visibility = View.GONE
@@ -48,7 +90,7 @@ class MainActivity : AppCompatActivity() {
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
             try {
-
+                /* Extracting JSON returns from the API */
                 val jsonObj = JSONObject(result)
                 val main = jsonObj.getJSONObject("main")
                 val sys = jsonObj.getJSONObject("sys")
@@ -70,6 +112,7 @@ class MainActivity : AppCompatActivity() {
 
                 val address = jsonObj.getString("name")+", "+sys.getString("country")
 
+                /* Populating extracted data into our views */
                 findViewById<TextView>(R.id.address).text = address
                 findViewById<TextView>(R.id.updated_at).text =  updatedAtText
                 findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
@@ -82,6 +125,7 @@ class MainActivity : AppCompatActivity() {
                 findViewById<TextView>(R.id.pressure).text = pressure
                 findViewById<TextView>(R.id.humidity).text = humidity
 
+                /* Views populated, Hiding the loader, Showing the main design */
                 findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
                 findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
 
@@ -93,95 +137,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-//import android.icu.text.SimpleDateFormat
-//import android.os.AsyncTask
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import android.view.View
-//import android.widget.ProgressBar
-//import android.widget.RelativeLayout
-//import android.widget.TextView
-//import org.json.JSONObject
-//import java.net.URL
-//import java.util.*
-//
-//class MainActivity : AppCompatActivity() {
-//
-//    val CITY: String="dhaka,bd"
-//    val API: String="881347715cccdd757623c4b7c8ec5343"
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        weatherTask().execute()
-//    }
-//    inner class weatherTask() : AsyncTask<String, Void, String>()
-//    {
-//        override fun onPreExecute(){
-//            super.onPreExecute()
-//            findViewById<ProgressBar>(R.id.loader).visibility= View.VISIBLE
-//            findViewById<RelativeLayout>(R.id.mainContainer).visibility=View.GONE
-//            findViewById<TextView>(R.id.errortext).visibility=View.GONE
-//        }
-//
-//        override fun doInBackground(vararg p0: String?): String? {
-//            var response:String?
-//            try{
-//                response= URL("https://api.openweathermap.org/data/2.5/weather?g=$CITY&unitis=metric&appid=$API")
-//                    .readText(Charsets.UTF_8)
-//            }
-//            catch(e:Exception)
-//            {
-//                response=null
-//            }
-//            return response
-//        }
-//        override fun onPreExceute(result: String?) {
-//            super.onPreExecute()
-//            try {
-//                val jsonObj = JSONObject(result)
-//                val main = jsonObj.getJSONObject("main")
-//                val sys = jsonObj.getJSONObject("sys")
-//                val wind = jsonObj.getJSONObject("wind")
-//                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-//                val updatedAt: Long = jsonObj.getLong("dt")
-//                val updateAtText =
-//                    "Update at: " + SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(
-//                        Date(updatedAt * 1000)
-//                    )
-//                val temp = main.getString("temp") + "℃"
-//                val tempMin = "Minimum temperature" + main.getString("temp_min") + "℃"
-//                val tempMax = "Maximum temperature" + main.getString("temp_max") + "℃"
-//                val pressure = main.getString("pressure")
-//                val humidity = main.getString("humidity")
-//                val sunrise: Long = sys.getLong("sunrise")
-//                val sunset: Long = sys.getLong("sunset")
-//                val windSpeed = wind.getString("speed")
-//                val weatherDescription = weather.getString("descriptoin")
-//                val address = jsonObj.getString("name") + ", " + sys.getString("country")
-//
-//
-//                findViewById<TextView>(R.id.address).text = address
-//                findViewById<TextView>(R.id.updated_at).text = updateAtText
-//                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
-//                findViewById<TextView>(R.id.temp).text = temp
-//                findViewById<TextView>(R.id.temp_min).text = tempMin
-//                findViewById<TextView>(R.id.temp_max).text = tempMax
-//                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise * 1000))
-//                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset * 1000))
-//                findViewById<TextView>(R.id.wind).text = windSpeed
-//                findViewById<TextView>(R.id.pressure).text = pressure
-//                findViewById<TextView>(R.id.humidity).text = humidity
-//
-//                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-//                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
-//        } catch (e: Exception) {
-//        findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-//        findViewById<TextView>(R.id.errorText).visibility = View.VISIBLE
-//    }
-//
-//    }
-//        }
-//    }
